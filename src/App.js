ebcc033d35993a755efa808365cd570ce820c2e6
zaqaz113 <zaqaz113@naver.com>
2023-02-09 15:01:24 +0900

    Modal

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  
-  let post = '강남 우동 맛집';
+
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동맛집','파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
+  
 
   return (
     <div className="App">
@@ -27,7 +28,10 @@ function App() {
       }}>글 제목 바꿔버리깅</button>
       
       <div className="list">
-        <h4>{글제목[0]} <span onClick={()=>{ 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h4>
+        <h4 onClick={() => {
+            setModal(!modal);
+
+        }}>{글제목[0]} <span onClick={()=>{ 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
@@ -38,6 +42,21 @@ function App() {
         <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      {
+        modal == true ? <Modal/> : null
+      }
+      
+    </div>
+  )
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   )
 }
