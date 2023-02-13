59e289edeb02cef8ae5c6aaef65c51b9d7ce0757
zaqaz113 <zaqaz113@naver.com>
2023-02-13 22:35:35 +0900

    블로그 글발행 기능, 삭제기능

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동맛집','파이썬 독학']);
   let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState('');
   
 
   return (
@@ -45,39 +47,68 @@ function App() {
 
       {
         글제목.map(function (a, i){
+          const date = new Date;
           return(
             <div className="list" key={i}>
               <h4 onClick={() => {
                 setModal(!modal);
+                setTitle(i);
               }}>{글제목[i]}
+                <span onClick={(e) => {
+                  e.stopPropagation()
+                  let copy = [...따봉]
+                  copy[i] += 1
+                  따봉변경(copy)
+                }}>👍{따봉[i]}</span>
               </h4>
-              <span onClick={() => {
-                let copy = [...따봉]
-                copy[i] += 1
-                따봉변경(copy)
-              }}>👍{따봉[i]}</span>
               <p>2월 17일 발행</p>
+              <button onClick={()=>{
+                let copy = [...글제목];
+                copy.splice(i,1);
+                글제목변경(copy)
+              }}>삭제</button>
             </div>
           )
         })
       }
+      
+      <input type="text" onChange={(e)=>{
+        입력값변경(e.target.value)
+      }} />
+      <button onClick={() => {
+        if(입력값 == '') {
+          alert('글자를 입력해 주세요')
+        } else {
+          let copy = [...글제목];
+          let like = [...따봉];
+          copy.unshift(입력값);
+          like.unshift(0);
+          글제목변경(copy)
+          따봉변경(like);
+        }
+      }}>추가하기</button>
 
       {
-        modal == true ? <Modal/> : null
+        modal == true ? <Modal title={title} 글제목={글제목} 글제목변경={글제목변경} /> : null
       }
+
       
     </div>
   )
 }
 
-function Modal() {
+
+
+function Modal(props) {
   return (
     <div className="modal">
-      <h4>제목</h4>
+      <h4>{props.글제목[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={() => {
+        props.글제목변경(['여자 코트 추천', '강남 우동맛집', '파이썬 독학'])
+      }}>글수정</button>
     </div>
   )
 }
-
 export default App;
